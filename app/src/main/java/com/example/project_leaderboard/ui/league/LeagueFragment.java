ad36337aeccfb5ddb678bc23d5200e7da27a4fee
ColineFardel <coline.fardel@gmail.com>
2020-03-08 17:01:37 +0100

    Search icon + Settings layout

--- app/src/main/java/com/example/project_leaderboard/ui/league/LeagueFragment.java
+++ app/src/main/java/com/example/project_leaderboard/ui/league/LeagueFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -18,6 +20,11 @@ public class LeagueFragment extends Fragment {
 
     private LeagueViewModel leagueViewModel;
 
+    private String[] leagues= getResources().getStringArray(R.array.league);
+    ListView list;
+    View v;
+
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         leagueViewModel =
@@ -30,6 +37,36 @@ public class LeagueFragment extends Fragment {
                 textView.setText(s);
             }
         });
+        v=root;
         return root;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        leagues = v.getResources().getStringArray(R.array.league);
+        list = (ListView) v.findViewById(R.id.list_leagues);
+
+        list.setAdapter(new ArrayAdapter<String>(v.getContext(),
+                android.R.layout.simple_list_item_1 , leagues){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view;
+                //If View doesn't exist create a new view
+                if (convertView == null) {
+                    // Create the Layout
+                    LayoutInflater inflater = getLayoutInflater();
+                    view = inflater.inflate(R.layout.fragment_league, parent, false);
+                } else {
+                    view = convertView;
+                }
+
+                //Add Text to the layout
+                TextView textView1 = (TextView) view.findViewById(R.id.list_text_league);
+                textView1.setText(leagues[position]);
+
+                return view;
+            }
+        });
     }
 }
